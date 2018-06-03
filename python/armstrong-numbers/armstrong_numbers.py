72b6c84a24d48965d53b715a0c9d61eaa9e0413c
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-06-03 22:19:11 +0900

    Update armstrong_numbers.py

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,10 @@
 def is_armstrong(number):
-    pass
+    strnum = str(number)
+    sum = 0
+    for i in strnum:
+        sum += int(i)**len(strnum)
+        
+    if sum == number:
+        return True
+    else:
+        return False

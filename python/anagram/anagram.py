363f5e9df08bab0f63979bda7193eb8784084656
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-06-03 22:24:20 +0900

    Update anagram.py

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,29 @@
+import copy
 def detect_anagrams(word, candidates):
-    pass
+    lowerword = word.lower()
+    h = [0] * 26
+    
+    a = []
+    for ch in lowerword:
+        h[ord(ch)-ord('a')] += 1
+    
+    for words in candidates:
+        temp = copy.deepcopy(h)
+        b = True
+        lowerwords = str(words).lower()
+        if lowerwords == lowerword:
+            continue
+        for ch in lowerwords:
+            if ch.isalpha():
+                temp[ord(ch)-ord('a')] -= 1
+        for elem in temp:
+            if elem != 0:
+                b = False
+                break
+                
+        if b == True:
+            a.append(words)
+        else:
+            continue
+    
+    return a

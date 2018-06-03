24186e0e4b33fbd9c4b2943d1fbfe86cdbf640e0
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-06-03 22:23:39 +0900

    Update difference_of_squares.py

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,19 @@
 def square_of_sum(count):
-    pass
+    sum = 0
+    for i in range(1, count + 1):
+        sum += i
+    
+    return sum**2
+        
 
 
 def sum_of_squares(count):
-    pass
+    sum = 0
+    for i in range(1, count + 1):
+        sum += i**2
+        
+    return sum
 
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)

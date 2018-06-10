5c2002c3351f925e8988d27f5150a8cd68f51cc4
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-06-10 21:57:54 +0900

    Update sieve.py

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,10 @@
 def sieve(limit):
-    pass
+    primes = [True] * (limit+1) 
+    p = 2
+    while p * p <= limit:
+        if primes[p]:
+            for i in range(p * 2, limit + 1, p):
+                primes[i] = False  
+        p += 1
+    primes = [element for element in range(2, limit+1) if primes[element]]
+    return primes 

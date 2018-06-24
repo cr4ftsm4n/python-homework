c889680a10015649709b2f4a6d560c7f8d4d378c
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-06-24 20:11:19 +0900

    Update grains.py

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,15 @@
 def on_square(integer_number):
-    pass
+    if integer_number < 1 or integer_number > 64:
+        raise ValueError("error")
+    else:
+        return 2**(integer_number-1)
 
 
 def total_after(integer_number):
-    pass
+    total = 0
+    if integer_number < 1 or integer_number > 64:
+        raise ValueError("error")
+    else:
+        for i in range(integer_number):
+            total += 2**i
+    return total

a277c7f5a047466c0fcd6df777ab69b581574a8c
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-06-24 20:10:15 +0900

    Update luhn.py

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,27 @@
 class Luhn(object):
+    card_num = ""
     def __init__(self, card_num):
-        pass
+        self.card_num = card_num.replace(" ","")
 
     def is_valid(self):
-        pass
+        total = 0
+        if self.card_num.isdigit() and len(self.card_num) > 1:
+            i = 1
+            for x in self.card_num[::-1]:
+                num = int(x)
+                if i%2 == 0:
+                    if num*2 >=10:
+                        total += (num*2 - 9)
+                    else:
+                        total += (num*2)
+                else:
+                    total += num
+                i += 1
+        else:
+            total = 1
+        
+        if total%10 == 0:
+            return True
+        else:
+            return False
+                        

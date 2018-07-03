2a447d9a558273d56e25729792abe801dea84b77
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-03 21:52:33 +0900

    Update prime_factors.py

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,13 @@
 def prime_factors(natural_number):
-    pass
+    n = natural_number
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors

3c09188dd40f7360aca25db110f0fcedbb028cb6
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-16 18:23:37 +0900

    Update matrix.py

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,21 @@
 class Matrix(object):
+    matrix_list = []
     def __init__(self, matrix_string):
-        pass
+        self.matrix_list = []
+        matrix_list = matrix_string.split("\n")
+        for i in matrix_list:
+            self.matrix_list.append(i.split(" "))
 
     def row(self, index):
-        pass
+        row_list = []
+        for i in self.matrix_list[index]:
+            row_list.append(int(i))
+            
+        return row_list
 
     def column(self, index):
-        pass
+        column_list = []
+        for i in self.matrix_list:
+            column_list.append(int(i[index]))
+            
+        return column_list

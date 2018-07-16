726ed6b871a30558605436883e223c25783e3768
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-16 18:26:51 +0900

    Update nth_prime.py

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,26 @@
 def nth_prime(positive_number):
-    pass
+    if positive_number < 1:
+        raise ValueError("error")
+    count = 0
+    prime = 2
+    while count != positive_number:
+        if prime == 2:
+            count += 1
+            if count == positive_number:
+                break
+            else:
+                prime += 1
+                continue
+        for i in range(1,prime):
+            if prime%i == 0 and i != 1:
+                prime += 2
+                continue
+            else:
+                if i == prime-1:
+                    count += 1
+                    if count == positive_number:
+                        break
+                    else:
+                        prime += 2
+    return prime
+        

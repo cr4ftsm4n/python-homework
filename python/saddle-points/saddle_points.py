d433b1afcadcbad30770389e85719e32586f4277
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-16 18:25:01 +0900

    Update saddle_points.py

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,13 @@
 def saddle_points(matrix):
-    pass
+    if not all([len(r) == len(matrix[0]) for r in matrix]):
+        raise ValueError('error')
+
+    points = []
+    for row_idx, row in enumerate(matrix):
+        row_max = max(row)
+        for col_idx, col in enumerate(row):
+            col_min = min(zip(*matrix)[col_idx])
+            if col == row_max and col == col_min:
+                points.append((row_idx, col_idx))
+
+    return set(points)

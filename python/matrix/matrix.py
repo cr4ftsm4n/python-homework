f390520d195c569b6e3cbee3f81deb062dae6b51
cr4ftsm4n <mou.hangun@gmail.com>
2018-07-19 17:58:56 +0900

    update repo

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,11 @@
 class Matrix(object):
-    def __init__(self, matrix_string):
-        pass
+    def __init__(self, s):
+        self.rows = [[int(n) for n in row.split()]
+                     for row in s.split('\n')]
+        self.columns = [list(tup) for tup in zip(*self.rows)]
 
     def row(self, index):
-        pass
+        return self.rows[index]
 
     def column(self, index):
-        pass
+        return self.columns[index]
--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,18 @@
 def saddle_points(matrix):
-    pass
+    pivot = list(zip(* matrix))
+    r = len(matrix)
+    if r == 0:
+        return set()
+    c = len(matrix[0])
+    for i in range(0, r):
+        if len(matrix[i]) != c:
+            raise ValueError("bad matrix")
+    result = []
+    for i in range(0, r):
+        for j in range(0, c):
+            this_row = matrix[i]
+            this_col = pivot[j]
+            point = matrix[i][j]
+            if point >= max(this_row) and point <= min(this_col):
+                result.append((i, j))
+    return set(result)

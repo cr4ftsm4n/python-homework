e676b73ecd032c774d2400c17e8765df30505175
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-22 16:13:20 +0900

    Update bracket_push.py

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -1,2 +1,24 @@
+open_brackets = '([{'
+close_brackets = ')]}'
+brackets_map = {')': '(', ']': '[', '}': '{'}
 def is_paired(input_string):
-    pass
+    stack = []
+    label = True
+    for char in input_string:
+        if char in open_brackets:
+            stack.append(char)
+        elif char in close_brackets:
+            if len(stack) < 1:
+                label = False
+                break
+            elif brackets_map[char] == stack[-1]:
+                stack.pop()
+            else:
+                label = False
+                break
+        else:
+            continue
+    if stack != []:
+        label = False
+        
+    return label

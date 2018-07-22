ad56a17af196dbb6079acbf96dfe05d86a620354
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-22 16:14:24 +0900

    Update secret_handshake.py

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,6 +1,35 @@
+ACTIONS = ['wink', 'double blink', 'close your eyes', 'jump']
+
 def handshake(code):
-    pass
+    l = []
+    num = code
+    while True:
+        num, remainder = divmod(num, 2)
+        l.append(remainder)
+        if num == 0:
+            break
+            
+    shake = []
+    for i in range(0,len(l)):
+        if l[i] == 1:
+            if i == 4:
+                shake = shake[::-1]
+            else:
+                shake.append(ACTIONS[i])
+                
+    return shake
+        
 
 
 def secret_code(actions):
-    pass
+    code = 0
+    for action in actions:
+        if action == ACTIONS[0]:
+            code += 1
+        elif action == ACTIONS[1]:
+            code += 2
+        elif action == ACTIONS[2]:
+            code += 4
+        elif action == ACTIONS[3]:
+            code += 8
+    return code

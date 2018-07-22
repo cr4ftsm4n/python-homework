f0983dfc7f26edc1946a775cd1cfdf5ed22c0620
zhangyan0814 <38320354+zhangyan0814@users.noreply.github.com>
2018-07-22 16:15:05 +0900

    Update perfect_numbers.py

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,13 @@
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError("error")
+    sum = 0
+    for divisor in range(1, number):
+        if not number % divisor:
+            sum += divisor
+    if sum == number:
+        return "perfect"
+    elif sum > number:
+        return "abundant"
+    else:
+        return "deficient"

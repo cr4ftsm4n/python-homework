ccbdba9652c77991eb066f9aff68f7bfd3e5034b
cr4ftsm4n <mou.hangun@gmail.com>
2018-07-26 14:10:10 +0900

    update answer

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,14 @@
 def classify(number):
-    pass
+    if number in [0, -1]:
+        raise ValueError("bad value")
+    total = []
+    for i in range(1, number):
+        if number % i == 0:
+            total.append(i)
+    s = sum(total)
+    if s == number:
+        return "perfect"
+    elif s < number:
+        return "deficient"
+    else:
+        return "abundant"
--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,6 +1,28 @@
 def handshake(code):
-    pass
+    actions = []
+    if code & 1 != 0:
+        actions.append("wink")
+    if code & 2 != 0:
+        actions.append("double blink")
+    if code & 4 != 0:
+        actions.append("close your eyes")
+    if code & 8 != 0:
+        actions.append("jump")
+    if code & 16 != 0:
+        actions = actions[::-1]
+    return actions
 
 
 def secret_code(actions):
-    pass
+    total = 0
+    possible = ["wink", "double blink", "close your eyes", "jump"]
+    for action in actions:
+        if action in possible:
+            idx = possible.index(action)
+            total += 2 ** (idx)
+    if len(actions) > 1:
+        fst = actions[0]
+        sec = actions[1]
+        if possible.index(fst) > possible.index(sec):
+            total += 16
+    return total
